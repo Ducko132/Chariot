1dad944f7bb7aced56bcb454bf2cb719efb54f2e
ducko132 <60584569+Ducko132@users.noreply.github.com>
2023-05-23 21:35:55 -0700

    UI Update
    added start game and next turn, so it's not that confusing. moved around some stuff.

--- UI.gd
+++ UI.gd
@@ -1,9 +1,18 @@
 extends Control
 
+var firstturn = true
 func _on_pass_turn_pressed():
 	StateMachine.next_state()
+	firstturn = false
 	pass # Replace with function body.
 
+func _ready():
+	if firstturn: 
+		$CanvasLayer/PassTurn.text = "Start the Game!"
+
 func _process(delta):
+	if !firstturn:
+		$CanvasLayer/PassTurn.text = "Next Turn"
+		
 	$CanvasLayer/MonsterHealth.text = "MONSTER HEALTH: " + str(StateMachine.monsterhp) + " MONSTER DOES: " + str(StateMachine.curmonstermove)
 	$CanvasLayer/PlayerHealth.text = "PLAYER HEALTH: " + str(StateMachine.playerhp) + " PLAYER MANA: " + str(StateMachine.playermana)
